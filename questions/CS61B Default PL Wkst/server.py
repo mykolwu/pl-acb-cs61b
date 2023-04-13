4f251e50f25a7bd0f0006b6ea4ea686b7b6cb62a
Michael Wu <michael.wu1@berkeley.edu>
2023-04-12 19:18:44 -0700

    4/12 update

--- questions/CS61B Default PL Wkst/server.py
+++ questions/CS61B Default PL Wkst/server.py
@@ -9,58 +9,40 @@ import prairielearn as pl
 
 
 def generate(data):
-    data["params"]["weight1"] = random.randint(1, 10)
-    data["params"]["weight2"] = random.randint(1, 10)
+    data["correct_answers"]["DFSPreorder"] = "9-0-1-6-5-2-7"
+    data["correct_answers"]["DFSPostorder"] = "0-2-7-5-6-1-9"
+    data["correct_answers"]["BFSOrder"] = "9-0-1-2-6-7-5"
 
-    mat = np.random.random((3, 3))
-    mat = mat / la.norm(mat, 1, axis=0)
-    data["params"]["labels"] = pl.to_json(["A", "B", "C"])
-    data["params"]["matrix"] = pl.to_json(mat)
-    data["params"]["symmetric_matrix"] = pl.to_json(np.maximum(mat, mat.T))
-
-    mat2 = np.random.binomial(1, 0.5, (3, 3))
-    data["params"]["matrix2"] = pl.to_json(mat2)
-
-    mat3 = np.array([[None, 2, -1.5], [-1.1, -1.4, None], [None, 4, -2]])
-    data["params"]["matrix3"] = pl.to_json(mat3)
-
-    # chosen by dice roll, guaranteed to be random
-    edge_mat = np.array([[-1, 0, 1, 0], [0, -1, 1, 0], [1, 0, 0, -1], [0, 1, -1, 0]])
-    data["params"]["edge-inc-mat"] = pl.to_json(edge_mat)
-
-    random_graph = nx.gnm_random_graph(5, 6)
-
-    for in_node, out_node, edge_data in random_graph.edges(data=True):
-        edge_data["label"] = random.choice(string.ascii_lowercase)
-
-    data["params"]["random-graph"] = pl.to_json(random_graph)
-
-    multigraph = nx.MultiDiGraph(
-        [(1, 2), (2, 1), (1, 2), (1, 3), (3, 2), (2, 3)], rankdir="LR"
-    )
-
-    data["params"]["multigraph"] = pl.to_json(multigraph)
-
-    # Generation code for color graph
-    subset_sizes = [5, 5, 4, 3, 2, 4, 4, 3]
-    subset_color = [
-        "gold",
-        "violet",
-        "violet",
-        "violet",
-        "violet",
-        "limegreen",
-        "limegreen",
-        "darkorange",
-    ]
-
-    extents = nx.utils.pairwise(itertools.accumulate((0,) + tuple(subset_sizes)))
-    layers = [range(start, end) for start, end in extents]
-    layered_graph = nx.Graph()
-    for i, (layer, color) in enumerate(zip(layers, subset_color)):
-        layered_graph.add_nodes_from(layer, layer=i, color=color)
-
-    for layer1, layer2 in nx.utils.pairwise(layers):
-        layered_graph.add_edges_from(itertools.product(layer1, layer2), color="blue")
-
-    data["params"]["color-graph"] = pl.to_json(layered_graph)
+    data["correct_answers"]["trie1"] = "U"
+    data["correct_answers"]["trie2"] = "E"
+    data["correct_answers"]["trie3"] = "N/A"
+    data["correct_answers"]["trie4"] = "F"
+    data["correct_answers"]["trie5"] = "I"
+    data["correct_answers"]["trie6"] = "G"
+    data["correct_answers"]["trie7"] = "M"
+    data["correct_answers"]["trie8"] = "A"
+    data["correct_answers"]["trie9"] = "T"
+    data["correct_answers"]["trie10"] = "R"
+    data["correct_answers"]["trie11"] = "U"
+    data["correct_answers"]["trie12"] = "E"
+    data["correct_answers"]["trie13"] = "N/A"
+    data["correct_answers"]["trie14"] = "N/A"
+    data["correct_answers"]["trie15"] = "N/A"
+    data["correct_answers"]["underlined"] = "2, 6, 8, 12"
+    
+    data["correct_answers"]["ab1"] = "U"
+    data["correct_answers"]["ab2"] = "E"
+    data["correct_answers"]["ab3"] = "N/A"
+    data["correct_answers"]["ab4"] = "F"
+    data["correct_answers"]["ab5"] = "I"
+    data["correct_answers"]["ab6"] = "G"
+    data["correct_answers"]["ab7"] = "M"
+    data["correct_answers"]["ab8"] = "A"
+    data["correct_answers"]["ab9"] = "T"
+    data["correct_answers"]["ab10"] = "R"
+    data["correct_answers"]["ab11"] = "U"
+    data["correct_answers"]["ab12"] = "E"
+    data["correct_answers"]["ab13"] = "N/A"
+    data["correct_answers"]["ab14"] = "N/A"
+    data["correct_answers"]["ab15"] = "N/A"
+    data["correct_answers"]["ab16"] = "N/A"
--- questions/Test Question/server.py
+++ questions/Test Question/server.py
@@ -1,18 +1,14 @@
 import random
+import numpy as np
+import prairielearn as pl
+import graphviz
 
 
 def generate(data):
 
-    # Sample two random integers between 5 and 10 (inclusive)
-    a = random.randint(5, 10)
-    b = random.randint(5, 10)
+    G = graphviz.Digraph('G')
+    G.attr('node', shape='triangle')
+    G.node('1', 'A')
+    G.node('2', 'B')
 
-    # Put these two integers into data['params']
-    data["params"]["a"] = a
-    data["params"]["b"] = b
-
-    # Compute the sum of these two integers
-    c = a + b
-
-    # Put the sum into data['correct_answers']
-    data["correct_answers"]["c"] = c
+    data["params"]["matrix"] = pl.to_json(G)
